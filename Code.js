18df03c268ffc16bfd367389682ab52a45c32950
vale630 <72229922+vale630@users.noreply.github.com>
2022-07-09 18:02:08 -0500

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCION";
+const accessCode3 = "BASE DE DATOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Pista: ¡Siempre cambiando, no es constante!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("U C O F N I N", 700,150)
+    fill("lightblue")
+    text("Pista: ¡Realiza una tarea en particular!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A E E A O B S D D T S", 100,250)
+    fill("lightblue")
+    text("Pista: ¡Guarda toda la información!", 100,270)
+
+}
